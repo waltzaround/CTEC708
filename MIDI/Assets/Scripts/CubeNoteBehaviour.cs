8d7ab90bbfeb1e107cf953670b8daf3db054f74a
Christopher Liu <christopherliu97@gmail.com>
2016-03-11 12:30:04 +1300

    Created simple primitives and placeholder code for control tests.

---
+++ MIDI/Assets/Scripts/CubeNoteBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeNoteBehaviour : MonoBehaviour {
+
+	public int NoteToPlay;
+
+	void onTriggerEnter (Collider other) {
+		if (other.tag == "Hand") {
+			Debug.Log (this.NoteToPlay);
+			// Sende la OSC
+		}
+	}
+}
