0d0bc46178db2cc9f63ecc743c4015b09c49b5ac
Christopher Liu <christopherliu97@gmail.com>
2016-03-14 13:44:44 +1300

    Added placeholder sound to Control type 3

--- MIDI/Assets/Scripts/CubeNoteBehaviour.cs
+++ MIDI/Assets/Scripts/CubeNoteBehaviour.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CubeNoteBehaviour : MonoBehaviour {
+public class CubeNoteBehaviour : MonoBehaviour 
+{
 
-	public int NoteToPlay;
+	//public int NoteToPlay;
 
-	void onTriggerEnter (Collider other) {
-		if (other.tag == "Hand") {
-			Debug.Log (this.NoteToPlay);
+	void OnTriggerEnter(Collider other) 
+	{
+		//Debug.Log ("Triggered");
+		if (other.gameObject.CompareTag("Hand"))
+		{
+			//Debug.Log (this.NoteToPlay);
+			//GetComponent("audio");
+			GetComponent<AudioSource>().Play ();
 			// Sende la OSC
 		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		GetComponent<AudioSource>().Pause ();
 	}
 }
