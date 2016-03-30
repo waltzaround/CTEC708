92b9412acda16a8391821157e5cfdbc79894758b
Christopher Liu <christopherliu97@gmail.com>
2016-03-30 15:25:14 +1300

    Created Leap-compliant buttons

--- MIDI/Assets/Scripts/HandButtonBehaviour.cs
+++ MIDI/Assets/Scripts/HandButtonBehaviour.cs
@@ -5,9 +5,15 @@ public class HandButtonBehaviour : MonoBehaviour {
 
 	//public Transform Pivot;
 	public GameObject Button;
-	private bool ButtonPressed;
+	public bool ButtonPressed;
 	public Transform Pressedpos;
 	public Transform Homepos;
+	public float pressDelay;
+	private float nextPress;
+	//private float startTime;
+	//private float journeyLength;
+	//public float speed;
+
 	//private bool ButtoninTransit;
 	//public int RotationSpeed;
 	//public int RotationDetent1;
@@ -16,14 +22,13 @@ public class HandButtonBehaviour : MonoBehaviour {
 	void Start () {
 		//rigidbody = GetComponent<Rigidbody> ();
 		ButtonPressed = false;
+		nextPress = 0;
+		//startTime = Time.time;
+		//journeyLength = Vector3.Distance(Homepos.position, Pressedpos.position);
 	}
 	
-	//void Update () {
-		/*if(ButtoninTransit == true)
-		{
-			//Debug.Log("Booled");
-			SmoothRotate();
-		}
+
+	/*
 		if (Button.GetComponent<Rigidbody>().rotation.x > RotationDetent1 && RotationSpeed > 0) 
 		{
 			Debug.Log ("STOP");
@@ -37,17 +42,40 @@ public class HandButtonBehaviour : MonoBehaviour {
 	//}
 
 	void FixedUpdate () {
-		if (Input.GetButton ("Fire1") ) {
+		if (ButtonPressed == true) {
+			//float distCovered = (Time.time - startTime) * speed;
+			//float fracJourney = distCovered / journeyLength;
+			//transform.position = Vector3.Lerp(Homepos.position, Pressedpos.position, fracJourney);
 			GetComponent<Rigidbody> ().MovePosition (Pressedpos.position);
 			//ButtonPressed = true;
 			/*Debug.Log ("Pressed");
 			RotationSpeed = -RotationSpeed;
 			ButtoninTransit = true;*/
 		} else {
+			//float distCovered = (Time.time - startTime) * speed;
+			//float fracJourney = distCovered / journeyLength;
+			//transform.position = Vector3.Lerp(Pressedpos.position, Homepos.position, fracJourney);
 			GetComponent<Rigidbody> ().MovePosition (Homepos.position);
 			//ButtonPressed = false;
 		}
 	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag ("Hand") && ButtonPressed == false && Time.time > nextPress) {
+			nextPress = Time.time + pressDelay;
+			ButtonPressed = true;
+		}
+		else if (other.gameObject.CompareTag ("Hand") && ButtonPressed == true && Time.time > nextPress) {
+			nextPress = Time.time + pressDelay;
+			ButtonPressed = false;
+		}
+	}
+
+	/*void OnTriggerExit (Collider other) {
+		if (other.gameObject.CompareTag ("Hand")) {
+			ButtonPressed = false;
+		}
+	}*/
 
 	/*void SmoothRotate () {
 		Button.transform.RotateAround (Pivot.position, Pivot.right, RotationSpeed * Time.deltaTime);
---
+++ MIDI/Assets/Scripts/TimerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerController : MonoBehaviour {
+
+	public Text textToDisplay;
+	private float millis;
+	private float sec;
+	private float min;
+	HandButtonBehaviour Playtab = GameObject.Find ("Playtab").GetComponent<HandButtonBehaviour> ();
+
+	// Use this for initialization
+	void Start () {
+		millis = 0;
+		sec = 0;
+		min = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textToDisplay.text = min.ToString () + ":" + sec.ToString () + ":" + millis.ToString ();
+		if (Playtab.ButtonPressed == true) {
+			startTheClock ();
+		}
+	}
+
+	void startTheClock() {
+		millis = millis + Time.deltaTime;
+	}
+}
